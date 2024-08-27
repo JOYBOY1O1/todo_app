fc91346364c1ab44c26952eae7849da559784893
Joy khulbe <khulbejoy.jk@gmail.com>
2024-08-27 11:48:40 +0530

    create bloc

--- lib/bloc/todo_bloc.dart
+++ lib/bloc/todo_bloc.dart
@@ -1,13 +1,27 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:todo_app/data/todo.dart';
 
 part 'todo_event.dart';
 part 'todo_state.dart';
 
-class TodoBloc extends Bloc<TodoEvent, TodoState> {
-  TodoBloc() : super(TodoInitial()) {
+class TodoBloc extends HydratedBloc<TodoEvent, TodoState> {
+  TodoBloc() : super(const TodoState()) {
     on<TodoEvent>((event, emit) {
       // TODO: implement event handler
     });
+  }
+
+  @override
+  TodoState? fromJson(Map<String, dynamic> json) {
+    // TODO: implement fromJson
+    throw UnimplementedError();
+  }
+
+  @override
+  Map<String, dynamic>? toJson(TodoState state) {
+    // TODO: implement toJson
+    throw UnimplementedError();
   }
 }
